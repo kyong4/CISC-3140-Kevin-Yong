7c0bbd83720ae4094bfa622031d95660e136c177
K Y <euler004@gmail.com>
2022-05-04 09:30:01 -0400

    dataDB added

--- lab4/index.js
+++ lab4/index.js
@@ -8,47 +8,46 @@ app.get("/",(req, res, next) => {
     res.json('Homepage for Lab 4 on localhost8000')
 });
 
-// Import SQLite3
+// Imports SQLite3
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('carInfo.db');
 
 app.use(express.json()); 
 
-//ROUTES-API ENDPOINTS
-//Root-Home
 app.get('/lab4/', (req, res)=>{	
   res.send('Lab 4');
 });
 
+//Display Cars
 app.get('/lab4/cars/', (req, res)=>{
-	res.send('Lasas');	
- // db.serialize(function(){
-//	  db.all("SELECT * FROM cars;", function(err, table){
-//		  res.json(table);
-	//})
-  //})
+
+ db.serialize(()=>{
+  db.all("SELECT * FROM cars;", (err, table)=>{
+		  res.json('table');
+	})
+  })
 });
 
-//Display Owners table from carInfo.db
-app.get('lab4/owners/', (req, res)=>{	
-  db.serialize(function(){
-	  db.all("SELECT * FROM owners;", function(err, table){
+//Display Owners
+app.get('/lab4/owners/', (req, res)=>{	
+  db.serialize(()=>{
+	  db.all("SELECT * FROM owners;", (err, table)=>{
 		  res.json(table);
 	})
   })
 });
-//Display Judges Table from carInfo.db
-app.get('lab4/judges/', (req, res)=>{	
-  db.serialize(function(){
-	  db.all("SELECT * FROM judges;", function(err, table){
+//Display Judges  
+app.get('/lab4/judges/', (req, res)=>{	
+  db.serialize(()=>{
+	  db.all("SELECT * FROM judges;", (err, table)=>{
 		  res.json(table);
 	})
   })
 });
-//Display Scores Table from carInfo.db
-app.get('lab4/scores/', (req, res)=>{	
-  db.serialize(function(){
-	  db.all("SELECT * FROM scores;", function(err, table){
+//Display Scores
+app.get('/lab4/scores/', (req, res)=>{	
+  db.serialize(()=>{
+	  db.all("SELECT * FROM scores;", (err, table)=>{
 		  res.json(table);
 	})
   })
